daddaeb7e03ea1de5b15fc2897bc9f285c53d060
sBaGeS <112703434+sBaGeS@users.noreply.github.com>
2022-09-07 23:28:29 +0300

    profiilin jatkamista

--- portfolio/src/components/profile/profile.js
+++ portfolio/src/components/profile/profile.js
@@ -11,7 +11,7 @@ export default function Profile() {
             <div id="profileRoot">
             <div className="float-container">
              <div id="koulutus" className='float-child'>
-             <h2 className='title'>Koulutus</h2>
+             <h2 id='title_koulutus'>Koulutus</h2>
              <a className="text">Peliohjelmoinnin insinööri. Kaakkois-Suomen ammattikorkeakoulu, Kotka 2022 - 2026.</a>
              <br/>
              <a className="text">Savonia Code Academy. Savonia-ammattikorkeakoulu, Kuopio 2020.</a>
@@ -19,7 +19,7 @@ export default function Profile() {
              <a className="text">Tietoliikennetekniikan perustutkinto. Savon ammattiopisto, Kuopio 2019.</a>
              </div>
              <div id="tekninen_osaaminen" className="float-child">
-             <h2 className='title'>Tekninen osaaminen</h2>
+             <h2 id='title_osaaminen'>Tekninen osaaminen</h2>
 
              <div id="osaaminen_gridContainer">
              <div className='osaaminen_item'>
@@ -85,8 +85,19 @@ export default function Profile() {
                 
              </div>
              <div id="harrastukset" className='float-child'>
-             <h2 className='title'>Harrastukset ja mielenkiinnon kohteet</h2>
-             <a className="text">aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</a>
+             <h2 id='title_harrastukset'>Harrastukset ja mielenkiinnon kohteet</h2>
+             <a className="text">Tykkään tietokoneista ja tietotekniikasta, suurin osa vapaa-ajastani 
+             kuluu tietokoneella istumiseen. <br/><br/>Tietokoneella ollessa tulee katseltua paljon youtubea, 
+             jonkin verran twitchiä ja joskus myös elokuvia ja sarjoja. Sarjoissa ja elokuvissa pidän
+             eniten toiminnasta ihan toimintakomedioista sinne hieman raaempaan päätyyn sekä scifistä.
+             Youtubea tykkään katsella todella laidasta laitaan, tietotekniikkaa, dokkareita, peleihin liittyviä 
+             videoita, draamaa. Käytännössä katselen kaikkea mikä nyt vain vaikuttaa sillä hetkellä mielenkiintoiselta<br/><br/>
+             Pelaan myös jonkin verran tietokonepelejä ja usein pelatessa tulee samalla striimattua pelaamistani 
+             twitchiin joskin koen hieman olevani kyllästynyt kun aikoinaan tullut pelattua todella paljon ellei 
+             jopa liikaa. Suosikki pelini on ehdottomasti CS:GO yli 4000 tuntia käytetty kyseiseen peliin ja hyvänä 
+             kakkosena minecraft johon en ole muutamaan vuoteen koskenut mutta on siihenkin tullut aikaa 
+             upotettua ainakin parintuhannen tunnin verran vuosien 2012-2020 välillä. <br/><br/>Luonteeltani olen introvertti 
+             ja viihdyn mielestäni hyvin yksin, olen myös rauhallinen ja rehellinen</a>
              </div>
             </div>
             </div>
