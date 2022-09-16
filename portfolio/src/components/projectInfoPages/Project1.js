b6ba7fa1c50f8b12fa7d9800265e27b2eab3133b
sBaGeS <112703434+sBaGeS@users.noreply.github.com>
2022-09-16 21:05:09 +0300

    projektisivujen työstämistä

--- portfolio/src/components/projectInfoPages/Project1.js
+++ portfolio/src/components/projectInfoPages/Project1.js
@@ -5,26 +5,44 @@ export default function Project1() {
 
         return (
             <div id="projectRoot">
+                <h1 id='project_title'>Projektin nimi</h1>        
             <div className="float_project">
 
-             <div id="tavoitteet" className='project_child'>
-             <h2 id='title_tavoitteet'>Projektin tavoitteet</h2>
-             <a className='text_project'>tähän projektin tavoitteet</a>
+             <div id="project1_info" className='project1_child'>
+             <h2 id='title_info1'>Projektin kuvaus</h2>
+             <a className='text_project'>Tämä projekti on fullstack nettisivu, eli se on jakautunut kolmeen eri
+             osa-alueeseen jotka ovat frontend, backend ja tietokanta. Frontend on toteutettu reactilla, 
+             backendi nodejs:llä ja tietokantana toimii mysql. <br/> <br/>
+             Projekti oli tarkoitus tehdä kolmen hengen ryhmissä Savonia Code Academyn lopputyöksi, tosin yksi meidän 
+             ryhmämme jäsen ei osallistunut projektin tekemiseen joten käytännössä meitä oli kaksi. <br/> <br/>
+             Projekti jakautui kuuteen kahden viikon pituiseen sprinttiin. Projektista tehtiin alkusuunnitelma 
+             Azuren Devopsiin. Suunnitelmassa määriteltiin toiminnallisuudet, mitkä niistä oli tarkoitus toteuttaa
+             missäkin sprintissä sekä työnjako eli kenen vastuulla minkäkin toiminnallisuuden tekeminen oli.</a>
              </div>
 
-             <div id="videoesittely" className="project_child">
-             <h2 id='title_video'>Videoesittely</h2>
-                <a className='text_project'>tähän upotettu video</a>
+             <div id="t1" className='project1_child'>
+             <h2 id='title_t1'>Tehtävän kuvaus</h2>
+             <a className="text_project">En muista ihan tarkasti mitkä kaikki toiminnallisuudet ovat minun tekemiä koska
+             minulla ei ole enää pääsyä projektin suunnitelmaan ja emme jakaneet tehtävänantoa niin karkeasti että
+             esimerkiksi toinen meistä tekisi backendin kokonaan ja toinen frontendin muun muassa sen takia koska halusimme
+             että molemmat meistä pääsevät tekemään sekä frontendiä että backendiä. <br/> <br/>
+             Tässä suuntaa-antavasti minun tehtäväni. Minun focus oli enemmän backendin puolella koska koin ja koen sen
+             edelleen mukavammaksi ja helpommaksi kuin frontin tekemisen joten karkeasti sanottuna noin 80% projektin backendistä
+             on minun tekemää sisältäen käyttäjän autentikoinnin backendissä tietokannasta tiedon hakemisen, muokkaamisen, lisäämisen
+             ja poistamisen yhteydessä sekä tietenkin suurimman osan reiteistä ja metodeista että tietokannan kanssa kommunikoiminen
+             on yleensäkään mahdollista. Tämän lisäksi autoin jonkunverran frontendin tekemisessä, tein ehkä noin puolet admin sivuista
+             muun muassa sivun jossa admin voi nähdä käyttäjien tietoja, hakea käyttäjiä erilaisilla hakuehdoilla, muokata käyttäjien tietoja
+             tai poistaa käyttäjiä. Tietokantamallin suunnittelimme yhdessä.</a>
              </div>
 
-             <div id="tekninen_toteutus" className='project_child'>
-             <h2 id='title_toteutus'>Tekninen toteutus</h2>
-             <a className="text_project">tähän tietoa teknisestä toteutuksesta</a>
+             <div id="funktiot1" className='project1_child'>
+             <h2 id='title_funktiot1'>Funktioiden kuvaukset</h2>
+             <a className="text_project"></a>
              </div>
 
-             <div id="toiminnallisuudet" className='project_child'>
-             <h2 id='title_toiminnallisuudet'>Projektin toiminnallisuudet</h2>
-             <a className="text_project">tähän tietoa projektin toiminnallisuuksista</a>
+             <div id="toiminnallisuudet" className='project1_child'>
+             <h2 id='title_toiminnallisuudet'>Ongelmat ja ratkaisut</h2>
+             <a className="text_project">tähän tietoa projektin ongelmista ja ratkaisuista</a>
              </div>
 
             </div>
--- portfolio/src/components/projectInfoPages/Project3.js
+++ portfolio/src/components/projectInfoPages/Project3.js
@@ -1,30 +1,44 @@
 import React, { useState } from 'react';
-import "./Project1.css"
+import "./Project3.css"
 
 export default function Project1() {
 
         return (
             <div id="projectRoot">
+            <h1 id='project_title'>Elorating systeemi csgo peliin</h1>   
             <div className="float_project">
 
-             <div id="tavoitteet" className='project_child'>
-             <h2 id='title_tavoitteet'>Projektin tavoitteet</h2>
-             <a className='text_project'>tähän projektin tavoitteet</a>
+             <div id="project3_info" className='project3_child'>
+             <h2 id="title_info3">Projektin Kuvaus</h2>
+             <a className='text_project'>Tämä projekti koostuu mysql tietokannasta johon tiedot tallennetaan sekä 
+             javascript tiedostoista jotka ovat erillisiä komentokehotteen tai powershellin avulla ajettavia 
+             nodejs scriptejä joilla kaikilla on oma tarkoituksensa datan
+             hakemiseen netistä, sen tallentamiseen, hakemiseen tietokannasta tai muokkamiseen. Tästä kerrottuna tarkemmin
+             funktioiden kuvaukset kappaleessa. <br/> <br/> Tämä projekti on kokonaan minun tekemä ja suunniteltu minulle
+             henkilökohtaiseen käyttöön, mikä näkyy muun muassa suunnitelman puuttumisena sekä projektin aikautauluissa. 
+             En jaksanut tehdä mitään suunnitelmaa koska en kokenut sitä tarpeellisena. Mitään varsinaista aikatauluakaan tällä
+             projektilla ei ole, edistyminen kulki ihan sen mukaan kuinka paljon minulla oli aikaa ja motivaatiota tämän projektin
+             tekemiseen eli joskus tein 10 tuntia päivässä ja joskus jätin hyllylle lojumaan x määräksi aikaa kun loppui motivaatio
+             tai aika.<br/> <br/> Koen tämän projektin muuten valmiiksi paitsi kaavaan jolla elolukuja päivitetään
+             saattaa vielä tulla joskus muutoksia jos koen että ennusteet eivät ole tarpeeksi paikkaansapitäviä. <br/> <br/>
+             Projektin tavoite on ennustaa tulevia CS:GO pelejä riittävän tarkasti että pystyn tekemään sen avulla rahaa
+             esimerkiksi lyömällä itse vetoa niihin peleihin tai myymällä ennusteita eteenpäin muiden käytettäväksi. 
+             Toistaiseksi en ole päässyt tuohon tavoitteeseen, teknisesti kaikki toimii mutta ennusteet eivät vain ole tarpeeksi
+             paikkaansapitäviä.
+             </a>
              </div>
 
-             <div id="videoesittely" className="project_child">
-             <h2 id='title_video'>Videoesittely</h2>
-                <a className='text_project'>tähän upotettu video</a>
+             <div id="funktiot3" className='project3_child'>
+             <h2 id='title_funktiot3'>Funktioiden kuvaukset</h2>
+             <a className="text_project">Datan keräämisen netistä toteutin Puppeteerin avulla joka on nodeen asennettava
+             moduuli joka sisältää koodilla ohjattavan chromium selaimen jota voi käyttää esimerkiksi datan keräämiseen
+             miltä tahansa nettisivulta eli webscrapeemiseen. Kaikki tähän projektiin tarvittava data on webscrapettu hltv.org
+             sivustolta puppeteerin avulla.</a>
              </div>
 
-             <div id="tekninen_toteutus" className='project_child'>
-             <h2 id='title_toteutus'>Tekninen toteutus</h2>
-             <a className="text_project">tähän tietoa teknisestä toteutuksesta</a>
-             </div>
-
-             <div id="toiminnallisuudet" className='project_child'>
-             <h2 id='title_toiminnallisuudet'>Projektin toiminnallisuudet</h2>
-             <a className="text_project">tähän tietoa projektin toiminnallisuuksista</a>
+             <div id="toiminnallisuudet" className='project3_child'>
+             <h2 id='title_toiminnallisuudet'>Ongelmat ja ratkaisut</h2>
+             <a className="text_project">tähän tietoa projektin ongelmista ja ratkaisuista</a>
              </div>
 
             </div>
