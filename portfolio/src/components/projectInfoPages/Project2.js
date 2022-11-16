d62b394e5ddeddf211261b92b7cbb5b3764461c5
sBaGeS <112703434+sBaGeS@users.noreply.github.com>
2022-11-16 23:50:28 +0200

    Travian botti sisältö

--- portfolio/src/components/projectInfoPages/Project2.js
+++ portfolio/src/components/projectInfoPages/Project2.js
@@ -5,7 +5,7 @@ export default function Project2() {
 
         return (
             <div className="projectRoot">
-            <h1 className='project_title'>Projektin nimi</h1>
+            <h1 className='project_title'>Travian botti</h1>
 
             <div className="project_box">
 
@@ -13,30 +13,65 @@ export default function Project2() {
              <div className='title_div'>
              <h2 className='title'>Projektin kuvaus</h2><br/><br/>
              </div>
-             <a className='text_project'>projektin kuvaus</a>
-             </div>
+             <a className='text_project'>Verkkoselainpeliä traviania varten tekemäni botti joka pelaa peliä puolestani.
+             Tämä projekti on kokonaan minun tekemä ja suunniteltu minulle henkilökohtaiseen käyttöön, mikä näkyy muun 
+             muassa suunnitelman puuttumisena sekä projektin aikautauluissa. En jaksanut tehdä mitään suunnitelmaa koska 
+             en kokenut sitä tarpeellisena. Mitään varsinaista aikatauluakaan tällä projektilla ei ole, edistyminen kulki 
+             ihan sen mukaan kuinka paljon minulla oli aikaa ja motivaatiota tämän projektin tekemiseen ja motivaatio
+             oli sidottu vahvasti minun travianin pelaamiseen sillä kun lopetin travianin pelaamisen lopetin myös tämän projektin jatkamisen.
+             Tämän projektin päätavoite oli vähentää travianiin käyttämääni aikaa ilman että jäisin muista pelaajista jälkeen.
+             Lisäksi halusin haastaa itseäni ja vastata päässäni olevaan kysymykseen riittääkö osaamiseni tälläisen tekemiseen?
+             <br/><br/>
+             Tämä projekti jäi mielestäni kesken. Rakennusten rakentamis ominaisuus jäi osittain kesken ja tämän lisäksi
+             tähän bottiin olisi voinut lisätä vaikka mitä ominaisuuksia ja visuaaliseenkin puoleen jäi vielä paljon tekemistä
+             sillä keskityin enimmäkseen vain toiminnallisuuksien tekemiseen.
 
-             <div className='project_child'>
-                
-             <div className='title_div'>
-             <h2 className='title'>Tehtävien Kuvaus</h2><br/><br/>
-             </div>
-             <a className="text_project">tehtävien kuvaus</a>
+             </a>
              </div>
 
              <div className='project_child'>
              <div className='title_div'>
              <h2 className='title'>Tärkeimmät toiminnallisuudet</h2><br/><br/>
              </div>
-             <a className="text_project">tietoa toiminallisuuksista</a>
+             <a className="text_project">Tämä botti on koodattu Electron frameworkilla käyttäen html:llää, css:ssää ja javascriptiä sekä
+             puppeteeria joka on javascript moduuli joka sisältää koodilla ohjattavan chromium selaimen jota voi käyttää esimerkiksi datan keräämiseen
+             miltä tahansa nettisivulta eli webscrapeemiseen ja tässä tapauksessa myös sivulla olevien nappuloiden painamiseen. Electronin avulla tein tästä botista ajattevan .exe windows sovelluksen.
+            <br/><br/>
+             Botille pitää ensin antaa
+             kaksi aikaa minuutteina, minimi ja maksimi, farmilistojen numerot sekä pelimaailman url osoite, pelaajan käyttäjätunnus ja salasana. Sen jälkeen sille voi lisätä rakennuksia rakennettavaksi
+             rakennusjonoon. Jonka jälkeen kun botin käynnistää painamalla start nappulaa botti kirjautuu käyttäjän
+             tilille, lähettää farmilistat ja laittaa rakennusjonossa seuraavana olevan rakennuksen rakentumaan jos mahdollista.
+             Tämän jälkeen botti arpoo randomilla numeron käyttäjän asettaman minimin ja maksimin väliltä ja odottaa arvotun numeron
+             ajan minuutteina jonka jälkeen sama toistuu, farmilistat, rakentaminen, uusi randomi odotusaika ja odottaminen.
+             </a>
+             </div>
+
+             <div className='project_child'>
+             <div className='title_div'>
+             <h2 className='title'>Ongelmat ja ratkaisut</h2><br/><br/>
+             </div>
+             <a className="text_project">Pienenä ongelmana oli aluksi Electronin käyttö johtuen kokemuksen puutteesta. Tästä
+             pääsin eteenpäin youtube tutoriaalien avulla. Tämän lisäksi tästä projektista on jäänyt hyvin mieleen toinenkin
+             ongelma, joka oli seuraavanlainen että kun ajoin botin komentokehotteen tai powershellin avulla niin kaikki toimi niinkuin
+             kuuluikin mutta kun tein siitä exen niin botti ei enää tehnyt mitään, tämä johtui siitä että Electron ei jostain syystä
+             toimi kunnolla puppeteerin sisältämän chromium selaimen kanssa ja tämän saa korjattua vaihtamalla puppeteer käyttämään
+             chromea chromiumin sijaan. Ratkaisun ongelmaan löysin googlettamalla ja minulla on vielä linkki kyseiseen artikkeliin 
+             tallessa jossa on ongelma sekä ratkaisu paremmin selitettynä.
+             
+             Tässä linkki kyseiseen artikkeliin <a className="text_project" href="https://medium.com/@alexanderruma/using-node-js-puppeteer-and-electronjs-to-create-a-web-scraping-app-with-a-desktop-interface-668493ced47d">https://medium.com/@alexanderruma/using-node-js-puppeteer-and-electronjs-to-create-a-web-scraping-app-with-a-desktop-interface-668493ced47d</a>
+             </a>
              </div>
 
              <div className='project_child bottom'>
              <div className='title_div'>
-             <h2 className='title'>Ongelmat ja ratkaisut</h2><br/><br/>
+             <h2 className='title'>Videoesittely</h2><br/><br/>
              </div>
-             <a className="text_project">ongelmat ja ratkaisut</a>
+             <a className="text_project">En ole tehnyt tästä projektista videoesittelyä sillä mielestäni tässä ei ole
+             tarpeeksi esiteltävää ja tämän lisäksi tarvitsisin esittelyä varten pelitilin jolla on ostettuna kultaklubi 
+             farmilistoja varten.
+             </a>
              </div>
+
 
             </div>
             </div>
