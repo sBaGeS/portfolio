1ad3f10782a7a7911943e55a65d0098aba1a6a56
sBaGeS <112703434+sBaGeS@users.noreply.github.com>
2022-11-17 10:53:27 +0200

    travian botin sisällön jatkoa

--- portfolio/src/components/projectInfoPages/Project2.js
+++ portfolio/src/components/projectInfoPages/Project2.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import "./Project.css"
+import kuva1 from "../../pictures/project2_pic1.png"
+import kuva2 from "../../pictures/project2_pic2.png"
 
 export default function Project2() {
 
@@ -35,7 +37,7 @@ export default function Project2() {
              </div>
              <a className="text_project">Tämä botti on koodattu Electron frameworkilla käyttäen html:llää, css:ssää ja javascriptiä sekä
              puppeteeria joka on javascript moduuli joka sisältää koodilla ohjattavan chromium selaimen jota voi käyttää esimerkiksi datan keräämiseen
-             miltä tahansa nettisivulta eli webscrapeemiseen ja tässä tapauksessa myös sivulla olevien nappuloiden painamiseen. Electronin avulla tein tästä botista ajattevan .exe windows sovelluksen.
+             miltä tahansa nettisivulta eli webscrapeemiseen ja tässä tapauksessa myös sivulla olevien nappuloiden painamiseen. Electronin avulla tein tästä botista ajettavan .exe windows sovelluksen.
             <br/><br/>
              Botille pitää ensin antaa
              kaksi aikaa minuutteina, minimi ja maksimi, farmilistojen numerot sekä pelimaailman url osoite, pelaajan käyttäjätunnus ja salasana. Sen jälkeen sille voi lisätä rakennuksia rakennettavaksi
@@ -44,6 +46,51 @@ export default function Project2() {
              Tämän jälkeen botti arpoo randomilla numeron käyttäjän asettaman minimin ja maksimin väliltä ja odottaa arvotun numeron
              ajan minuutteina jonka jälkeen sama toistuu, farmilistat, rakentaminen, uusi randomi odotusaika ja odottaminen.
              </a>
+
+             
+             <div className="picture_grid large">
+             <div>
+           
+             <img src={kuva1} className="picture"></img>
+
+             <a className="text_project">Tässä kuvassa näkyy koko botin käyttöliittymä. Vasemmanpuolimmaisesta
+             erillisestä ikkunasta lisätään botille rakennuksia rakennusjonoon jotka näkyvät keskellä olevassa pääikkunassa 
+             tasks laatikon sisällä, lisäsin sinne muutaman rakennuskäskyn malliksi. Keskellä olevassa pääikkunassa näkyy
+             ylhäällä nappuloita joista saa nuo muut ikkunat aukaistua, startattua ja stopattua botin sekä poistuttua botista
+             sulkemalla se kokonaan. State kohdassa näkyy botin tämänhetkinen tila eli onko se päällä vai stopattuna. Logs kohdan
+             alla näkyy mitä botti on milloinkin tehnyt ja tasks kohdan alla näkyy botille annettu rakennusjono. Oikeanpuolimmaiseen
+             laatikkoon tulee käyttäjän tiedot että botti osaa toimia. Useragent laatikkoon copypastetaan käyttäjän jonkun muun
+             selaimen useragent jotta botti vaikkuttaisi vähemmän bottimaiselta joka vähentää kiinnijäämisen riskiä sillä bottien
+             käyttäminen on kielletty travianin terms of servicessä. Server kohtaan tulee serverin sisäänkirjautumis-sivun url osoite.
+             Username ja Password kentät ovat itsestäänselviä. Farmlists kohtaan farmilistojen numerot jotta botti osaa lähettää ne.
+             Interval kenttiin minimi ja maksimi että kuinka tiheästi haluaa botin pelaavan joiden avulla botti arpoo randomilla
+             joka odotuskohdassa uuden odotusajan, tämä tehty samasta syystä kuin useragent kenttä. Headless kohdasta käyttäjä
+             saa valita haluaako nähdä selaimen jota botti ohjaa vai pidetäänkö se piilossa taustatehtävänä. Select nappia painamalla
+             käyttäjän pitää vielä käydä valitsemassa chrome.exe, muuten botti ei toimi, tästä selitettynä paremmin alempana
+             Ongelmat ja ratkaisut kohdassa. Save nappula tallentaa tiedot erilliseen json tiedostoon json muodossa josta botti osaa
+             ne lukea ettei näitä samoja tietoja joudu aina syöttämään uudelleen botin avaamisen yhteydessä, myös botin rakennusjono
+             tallentuu erilliseen json tiedostoon samasta syystä.
+             </a>
+             </div>
+
+             </div>
+
+             <div className="picture_grid large">
+             <div>
+           
+             <img src={kuva2} className="picture"></img>
+
+             <a className="text_project">Tässä näkyy minun vanhat käyttäjätiedot botin tallentamana config.json tiedostoon,
+             paitsi salasana jonka poistin ennen kuvan ottamista tietoturvasyistä. Tämä botti ei ole kovin tietoturvallinen
+             ainakaan siinä mielessä että käyttäjän salasana tallennetaan sellaisenaan muun datan mukana json tiedostoon
+             josta se on helposti nähtävissä. Ilman salasanan antamista botin tietoon se ei tietenkään voi toimia mutta näin
+             jälkeenpäin ajateltuna olisi ehkä ollut järkevämpää että salasanaa ei tallennettaisi ollenkaan ja botti kysyisi
+             sen aina käyttäjältä uudelleen avaamisen yhteydessä.
+             </a>
+             </div>
+
+             </div>
+
              </div>
 
              <div className='project_child'>
